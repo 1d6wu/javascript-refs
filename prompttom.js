609001e0070bd24e60265ac42aedbc41cc3b5f75
1d6wu <tomidowu01@gmail.com>
2025-05-15 11:58:44 +0100

    add code from board

---
+++ prompttom.js
@@ -0,0 +1,11 @@
+let userPrompt = prompt("Choose a number :)");
+
+console.log("userPrompt = " + userPrompt);
+console.log("userPrompt type = " + typeof userPrompt);
+
+console.log(parseFloat(userPrompt));
+console.log(parseInt(userPrompt));
+
+
+
+
---
+++ script.js
@@ -0,0 +1,48 @@
+/*console.log("hi");
+
+const multiLineSentence = "this is a \n sentence" ; //\n is a linebreak
+console.log(multiLineSentence);
+
+const insertedVariable = "INSERTED VARIABLE";
+const variableSentence = `This is an ${insertedVariable}`;
+console.log("This is an " + insertedVariable);
+
+console.log(variableSentence.length); 
+
+
+let userInput = "hEllO tOm";
+let lowerCase = userInput.toLowerCase(); //forces string into lower case
+console.log(lowerCase);
+
+
+let fullSentence = "This is a long sentence for doing things like this string ksascha manipulation.";
+let variableCase = fullSentence[0].toLowerCase();
+
+console.log(fullSentence[0].toLowerCase()); //can be used to check length of users password etc
+
+let includesLong = fullSentence.includes("long"); //should convert sentence to upper or lower case as case sensitive.
+console.log(includesLong);
+
+let getSaschaOut = fullSentence.slice(59,65);
+console.log(getSaschaOut);
+
+let replaceSascha = fullSentence.replace("sascha","mike");
+console.log(replaceSascha);
+
+*/
+
+
+
+
+
+// Math object
+
+let useMath = Math.ceil(4.2); //rounds up = 5 
+let useMath2 = Math.floor(4.2); //rounds down = 4
+let useMath3  = Math.round(4.2); //actually rounds to 4
+console.log(useMath2);
+
+let randomNum = Math.random() * 20 + 1; // random number between 1 and 20.9
+let randomFloor = Math.floor(randomNum) // random number between 1 and 20
+console.log(randomNum);
+console.log(randomFloor)
